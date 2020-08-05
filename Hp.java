ead82734ec7492eadd28c683ab2e8b2b8a260a6b
shtandon <shrutitandon93@gmail.com>
2020-08-05 13:54:23 +0530

    v1

--- Hp.java
+++ Hp.java
@@ -3,7 +3,7 @@ public class PracticeHP {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	
-		System.out.println(""harry potter);
+		System.out.println("harry potter");
 	}
 
 }
